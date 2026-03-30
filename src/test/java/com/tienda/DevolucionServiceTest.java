4e3c054f3748b496927e9f7b6ef6046afc565574
Flor Cahuana <santananayely02@gmail.com>
2026-03-30 18:19:53 -0500

    test fecha invalida

--- src/test/java/com/tienda/DevolucionServiceTest.java
+++ src/test/java/com/tienda/DevolucionServiceTest.java
@@ -12,6 +12,17 @@ public class DevolucionServiceTest {
     DevolucionService service = new DevolucionService();
 
     @Test
+    void testCodigoDevolucionInvalido() {
+        String res = service.registrarDevoluciones(
+                "1234","ABC123","Jose",
+                "Producto fallado",
+                LocalDate.now().minusDays(1),
+                LocalDate.now());
+
+        assertEquals("Ingrese un código de devolución válido", res);
+    }
+
+    @Test
     void testCodigoProductoInvalido() {
         String res = service.registrarDevoluciones(
                 "D1234","ABC12","Jose",
@@ -20,5 +31,51 @@ public class DevolucionServiceTest {
                 LocalDate.now());
 
         assertEquals("Ingrese un código de producto válido", res);
+    }
+
+    @Test
+    void testNombreInvalido() {
+        String res = service.registrarDevoluciones(
+                "D1234","ABC123","J1",
+                "Producto fallado",
+                LocalDate.now().minusDays(1),
+                LocalDate.now());
+
+        assertEquals("El nombre del cliente debe tener al menos cuatro caracteres alfabéticos", res);
+    }
+
+    @Test
+    void testMotivoInvalido() {
+        String res = service.registrarDevoluciones(
+                "D1234","ABC123","Jose",
+                "Corto",
+                LocalDate.now().minusDays(1),
+                LocalDate.now());
+
+        assertEquals("El motivo de la devolución debe tener al menos diez caracteres", res);
+    }
+
+    @Test
+    void testFechaInvalida() {
+        String res = service.registrarDevoluciones(
+                "D1234","ABC123","Jose",
+                "Producto defectuoso",
+                LocalDate.now(),
+                LocalDate.now().minusDays(1));
+
+
+        assertEquals("Ingrese una fecha de devolución válida", res);
+    }
+
+    @Test
+    void testRegistroCorrecto() {
+        String res = service.registrarDevoluciones(
+                "D1234","ABC123","Jose",
+                "Producto defectuoso",
+                LocalDate.now(),
+                LocalDate.now().minusDays(2));
+
+
+        assertEquals("La devolución ha sido registrada correctamente", res);
     }
 }
